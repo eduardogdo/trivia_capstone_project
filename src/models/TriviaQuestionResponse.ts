5d03eb46871c1b484875c69b42abeb595fad2694
Rohit Raman <ramangaya.iem@gmail.com>
2022-10-19 18:23:26 -0400

    Added Question component

--- src/models/TriviaQuestionResponse.ts
+++ src/models/TriviaQuestionResponse.ts
@@ -1,6 +1,7 @@
 export default interface TriviaQuestionResponse {
   category: string;
   id: string;
+  options: string[];
   correctAnswer: string;
   incorrectAnswers: string[];
   question: string;
