59a8f9b1641a4414cc31670c25d46500b46caf80
Eduardo Gómez Dorado <eduardo.gomezdorado@hotmail.com>
2022-10-20 19:07:37 -0400

    upload header from config

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import QuestionContext from "../../context/QuestionContext";
 import TriviaQuestionResponse from "../../models/TriviaQuestionResponse";
 import { setScore } from "../../services/ScoreService";
@@ -11,6 +11,7 @@ interface Props {
 
 const Question = ({ question, moveToNextQuestion }: Props) => {
   const { score, addScore } = useContext(QuestionContext);
+  const [options, setOptions] = useState<string[]>([]);
 
   const answerQuestion = (answer: string) => {
     if (answer === question.correctAnswer) {
@@ -21,6 +22,33 @@ const Question = ({ question, moveToNextQuestion }: Props) => {
     }
 
     moveToNextQuestion();
+  };
+
+  useEffect(
+    function () {
+      setOptions(shuffle(question.options));
+    },
+    [question.options]
+  );
+
+  const shuffle = (array: string[]) => {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
   };
 
   return (
--- src/services/ScoreService.ts
+++ src/services/ScoreService.ts
@@ -1,10 +1,6 @@
 import axios from "../utils/api.client";
 import Score from "../models/ScoreModel";
 
-const headers = {
-  "x-apikey": "63509630626b9c747864ad82",
-};
-
 export const setScore = (score: Score) => {
   return axios.post("/score", score).then((response) => {
     console.log(response);
@@ -12,21 +8,13 @@ export const setScore = (score: Score) => {
 };
 
 export const getScore = (): Promise<Score[]> => {
-  return axios
-    .get("/score", {
-      headers: headers,
-    })
-    .then((response) => {
-      return response.data;
-    });
+  return axios.get("/score").then((response) => {
+    return response.data;
+  });
 };
 
 export const deleteScore = (id: string): Promise<any> => {
-  return axios
-    .delete(`/score/${id}`, {
-      headers: headers,
-    })
-    .then((response) => {
-      return response.data;
-    });
+  return axios.delete(`/score/${id}`).then((response) => {
+    return response.data;
+  });
 };
